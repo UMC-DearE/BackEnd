7232a5ec5c5bc86af9e7ea42e7c498d663e164f7
choehyeonjin <guswls2616@naver.com>
2026-01-20 23:43:13 +0900

    feat: JPA Auditing 설정 및 BaseEntity 추가

--- src/main/java/com/deare/backend/BackendApplication.java
+++ src/main/java/com/deare/backend/BackendApplication.java
@@ -2,15 +2,17 @@ package com.deare.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         // ▼▼▼ 이 줄을 추가해서 실행 창(Console) 로그를 확인해 주세요 ▼▼▼
         System.out.println(">>> CHECK DB_URL: " + System.getenv("DB_URL"));
 
         SpringApplication.run(BackendApplication.class, args);
-	}
+    }
 
 }
