d059ba6e986dd2496c614cbccf3690a4420b88e1
hajimeong <ha_01@naver.com>
2026-01-21 21:38:34 +0900

    refactor: FeignClient에 설정을 환경별 설정으로 수정

--- src/main/java/com/deare/backend/global/external/ai/client/AiTextFeignClient.java
+++ src/main/java/com/deare/backend/global/external/ai/client/AiTextFeignClient.java
@@ -3,6 +3,8 @@ package com.deare.backend.global.external.ai.client;
 import com.deare.backend.global.external.ai.dto.request.AiAnalyzeRequest;
 import com.deare.backend.global.external.ai.dto.response.AiAnalyzeResponse;
 import com.deare.backend.global.external.feign.config.FeignConfig;
+import com.deare.backend.global.external.feign.config.FeignDevConfig;
+import com.deare.backend.global.external.feign.config.FeignProdConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(
         name="aiTextClient",
         url="${external.ai.base-url}",
-        configuration = FeignConfig.class
+        configuration = {
+                FeignConfig.class,
+                FeignDevConfig.class,
+                FeignProdConfig.class
+        }
 )
 public interface AiTextFeignClient {
     @PostMapping(
