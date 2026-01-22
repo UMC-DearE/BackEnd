3a1855f8c5168c62645ac3b93c6a476a26d377d6
hajimeong <ha_01@naver.com>
2026-01-22 23:12:25 +0900

    feat: EmotionCategory 감정 대분류 엔티티 구현

---
+++ src/main/java/com/deare/backend/domain/emotion/entity/EmotionCategory.java
@@ -0,0 +1,32 @@
+package com.deare.backend.domain.emotion.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name="emotion_category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EmotionCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="category_id")
+    private Long id;
+
+    @Column(name="type", nullable = false)
+    private String type;
+
+    @Column(name="bg_color", nullable = false)
+    private String bgColor;
+
+    @Column(name="font_color", nullable = false)
+    private String fontColor;
+
+    private EmotionCategory(String type, String bgColor, String fontColor) {
+        this.type = type;
+        this.bgColor = bgColor;
+        this.fontColor = fontColor;
+    }
+}
