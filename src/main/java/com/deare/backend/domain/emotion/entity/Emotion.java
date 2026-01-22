7078081469518cbce05987dd9087ec0400158b14
hajimeong <ha_01@naver.com>
2026-01-22 23:13:14 +0900

    feat: 감정 엔티티 구현 및 감정대분류 엔티티연관관계 설정

---
+++ src/main/java/com/deare/backend/domain/emotion/entity/Emotion.java
@@ -0,0 +1,33 @@
+package com.deare.backend.domain.emotion.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name="emotion")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Emotion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="emotion_id")
+    private Long id;
+
+    @Column(name="name", nullable = false, length = 20)
+    private String name;
+
+    @ManyToOne(fetch=FetchType.LAZY, optional = false)
+    @JoinColumn(name="categry_id", nullable = false)
+    private EmotionCategory emotionCategory;
+
+    private Emotion(String name, EmotionCategory emotionCategory) {
+        this.name = name;
+        this.emotionCategory = emotionCategory;
+    }
+
+    public static Emotion create(String name, EmotionCategory emotionCategory) {
+        return new Emotion(name, emotionCategory);
+    }
+}
