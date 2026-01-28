6477dd79711c8409088481399750449d13f471d1
hajimeong <ha_01@naver.com>
2026-01-29 01:46:58 +0900

    feat: GeminiFeignClient에 OCR 전용 chatOcr API 추가 구현

--- src/main/java/com/deare/backend/global/external/gemini/client/GeminiFeignClient.java
+++ src/main/java/com/deare/backend/global/external/gemini/client/GeminiFeignClient.java
@@ -3,7 +3,8 @@ package com.deare.backend.global.external.gemini.client;
 import com.deare.backend.global.external.feign.config.FeignConfig;
 import com.deare.backend.global.external.feign.config.FeignDevConfig;
 import com.deare.backend.global.external.feign.config.FeignProdConfig;
-import com.deare.backend.global.external.gemini.dto.request.GeminiTextRequestDTO;
+import com.deare.backend.global.external.gemini.dto.request.analyze.GeminiTextRequestDTO;
+import com.deare.backend.global.external.gemini.dto.request.ocr.GeminiOcrRequestDTO;
 import com.deare.backend.global.external.gemini.dto.response.GeminiTextResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
@@ -26,8 +27,17 @@ public interface GeminiFeignClient {
             value = "/chat/completions",
             consumes = MediaType.APPLICATION_JSON_VALUE
     )
-    GeminiTextResponseDTO chat(
+    GeminiTextResponseDTO chatText(
             @RequestHeader("Authorization") String authorization,
             @RequestBody GeminiTextRequestDTO request
+    );
+
+    @PostMapping(
+            value = "/chat/completions",
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    GeminiTextResponseDTO chatOcr(
+            @RequestHeader("Authorization") String authorization,
+            @RequestBody GeminiOcrRequestDTO request
     );
 }
