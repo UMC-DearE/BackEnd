c3db19be580febea6a62321ad256eed2397fbdd2
catomat0 <catomat0@github.com>
2026-01-29 22:12:51 +0900

    Feat: 사용자 프로바이더 조회 메서드 추가

--- src/main/java/com/deare/backend/domain/user/repository/UserRepository.java
+++ src/main/java/com/deare/backend/domain/user/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.deare.backend.domain.user.repository;
 
+import com.deare.backend.domain.user.entity.Provider;
 import com.deare.backend.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByProviderAndProviderId(Provider provider, String providerId);
 }
