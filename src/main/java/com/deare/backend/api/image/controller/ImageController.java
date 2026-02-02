117170540179160f7fb12c506b958d7fe18aab30
Minsu Kwon <kms37480@naver.com>
2026-02-02 20:19:22 +0900

    피드백 수정 및 CItest 통과 로직 수정

--- src/main/java/com/deare/backend/api/image/controller/ImageController.java
+++ src/main/java/com/deare/backend/api/image/controller/ImageController.java
@@ -4,12 +4,14 @@ import com.deare.backend.api.image.dto.ImageUploadResponseDTO;
 import com.deare.backend.api.image.service.ImageUploadService;
 import com.deare.backend.global.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/images")
+@ConditionalOnProperty(name = "aws.s3.enabled", havingValue = "true")
 public class ImageController {
 
     private final ImageUploadService imageUploadService;
--- src/main/java/com/deare/backend/global/config/TestS3Config.java
+++ src/main/java/com/deare/backend/global/config/TestS3Config.java
@@ -1,28 +1,27 @@
 package com.deare.backend.global.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import org.springframework.context.annotation.Profile;
-
-
 @Configuration
+@Profile("test")
 @EnableConfigurationProperties(S3Properties.class)
-@ConditionalOnProperty(name = "aws.s3.enabled", havingValue = "true")
-
-public class S3Config {
+public class TestS3Config {
 
     @Bean
+    @Primary
     public DefaultCredentialsProvider defaultCredentialsProvider() {
         return DefaultCredentialsProvider.create();
     }
 
     @Bean
+    @Primary
     public S3Client s3Client(S3Properties props, DefaultCredentialsProvider cp) {
         return S3Client.builder()
                 .region(Region.of(props.region()))
@@ -31,6 +30,7 @@ public class S3Config {
     }
 
     @Bean
+    @Primary
     public S3Presigner s3Presigner(S3Properties props, DefaultCredentialsProvider cp) {
         return S3Presigner.builder()
                 .region(Region.of(props.region()))
