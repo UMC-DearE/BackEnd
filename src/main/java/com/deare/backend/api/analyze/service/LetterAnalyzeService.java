0e2c6a12c36b9520eacb666fb121629205cc91a3
hajimeong <ha_01@naver.com>
2026-02-02 19:49:05 +0900

    refactor: LetterAnalyzeSerive에 readOnly 트랜잭션 설정

--- src/main/java/com/deare/backend/api/analyze/service/LetterAnalyzeService.java
+++ src/main/java/com/deare/backend/api/analyze/service/LetterAnalyzeService.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class LetterAnalyzeService {
 
