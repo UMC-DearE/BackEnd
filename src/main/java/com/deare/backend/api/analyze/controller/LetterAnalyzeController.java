8b1667280017f58bd8dfc2d8848392f4e16197bf
hajimeong <ha_01@naver.com>
2026-02-02 19:34:35 +0900

    refactor: 편지 분석 API 엔드포인트 /analyze/letters로 변경

--- src/main/java/com/deare/backend/api/analyze/controller/LetterAnalyzeController.java
+++ src/main/java/com/deare/backend/api/analyze/controller/LetterAnalyzeController.java
@@ -14,11 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/letters")
+@RequestMapping("/analyze")
 public class LetterAnalyzeController {
     private final LetterAnalyzeService letterAnalyzeService;
 
-    @PostMapping("/analyze")
+    @PostMapping("/letters")
     @Operation(
             summary = "편지 요약 및 감정 태그 분석",
             description = "편지글을 받아 ai를 통해 편지글 한줄 요약 및 감정 태그를 추출하여 반환하는 API"
