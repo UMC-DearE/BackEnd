1a215cd6fe0706b9be47863f3daf9e9d45215390
ownue <enu_i@naver.com>
2026-02-02 21:52:55 +0900

    fix: 매개변수 누락 추가

--- src/main/java/com/deare/backend/api/letter/service/LetterServiceImpl.java
+++ src/main/java/com/deare/backend/api/letter/service/LetterServiceImpl.java
@@ -44,6 +44,7 @@ public class LetterServiceImpl implements LetterService {
                 userId,
                 folderId,
                 fromId,
+                isLiked,
                 keyword,
                 pageable
         );
