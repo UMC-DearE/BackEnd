a73cee9b6dc0eabc825983cc55d5ad4289089527
choehyeonjin <guswls2616@naver.com>
2026-02-03 11:58:07 +0900

    chore: S3 빈 로딩 분리

--- src/main/java/com/deare/backend/api/ocr/service/S3ImageContentLoader.java
+++ src/main/java/com/deare/backend/api/ocr/service/S3ImageContentLoader.java
@@ -2,10 +2,12 @@ package com.deare.backend.api.ocr.service;
 
 import com.deare.backend.global.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "aws.s3.enabled", havingValue = "true")
 public class S3ImageContentLoader implements ImageContentLoader {
 
     private final S3Service s3Service;
