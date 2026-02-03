1ef599d5f2b4ed823a49565aaeac8a2ef353741e
choehyeonjin <guswls2616@naver.com>
2026-02-03 09:19:45 +0900

    fix: OCR API 엔드포인트 수정

--- src/main/java/com/deare/backend/api/ocr/controller/LetterOcrController.java
+++ src/main/java/com/deare/backend/api/ocr/controller/LetterOcrController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/test/letters")
+@RequestMapping("/letters")
 public class LetterOcrController {
 
     private final LetterOcrService letterOcrService;
