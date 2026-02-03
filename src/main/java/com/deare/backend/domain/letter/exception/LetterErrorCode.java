4f4af19537421239baa74333c59a5ae520582974
ownue <enu_i@naver.com>
2026-02-03 09:52:10 +0900

    chore: 커스텀 에러코드 enum과 code 구분하여 enum명 변경

--- src/main/java/com/deare/backend/domain/letter/exception/LetterErrorCode.java
+++ src/main/java/com/deare/backend/domain/letter/exception/LetterErrorCode.java
@@ -9,43 +9,48 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum LetterErrorCode implements BaseErrorCode {
 
-    LETTER_40001(
+    INVALID_REQUEST(
             HttpStatus.BAD_REQUEST,
             "LETTER_40001",
             "잘못된 요청입니다."
     ),
+    FROM_REQUIRED(
+            HttpStatus.BAD_REQUEST,
+            "LETTER_40002",
+            "편지의 From은 필수입니다."
+    ),
 
-    LETTER_40101(
+    UNAUTHORIZED(
             HttpStatus.UNAUTHORIZED,
             "LETTER_40101",
             "로그인이 필요한 요청입니다."
     ),
 
-    LETTER_41001(
+    DELETED_LETTER(
             HttpStatus.GONE,
             "LETTER_41001",
             "삭제된 편지입니다."
     ),
 
-    LETTER_40301(
+    FORBIDDEN(
             HttpStatus.FORBIDDEN,
             "LETTER_40301",
             "해당 편지에 접근할 권한이 없습니다."
     ),
 
-    LETTER_40401(
+    NOT_FOUND(
             HttpStatus.NOT_FOUND,
             "LETTER_40401",
             "편지를 찾을 수 없습니다."
     ),
 
-    LETTER_50001(
+    INTERNAL_ERROR(
             HttpStatus.INTERNAL_SERVER_ERROR,
             "LETTER_50001",
             "처리 중 서버 오류가 발생했습니다."
     ),
 
-    LETTER_50002(
+    SUMMARY_INTERNAL_ERROR(
             HttpStatus.INTERNAL_SERVER_ERROR,
             "LETTER_50002",
             "편지 요약 처리 중 내부 오류가 발생했습니다."
