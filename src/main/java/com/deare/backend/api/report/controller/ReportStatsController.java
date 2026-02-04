34af42aed3a1e52978f914716b57e877e13508a2
hajimeong <ha_01@naver.com>
2026-02-04 18:15:39 +0900

    docs: Report 통계 조회 API Swagger 설명 추가

--- src/main/java/com/deare/backend/api/report/controller/ReportStatsController.java
+++ src/main/java/com/deare/backend/api/report/controller/ReportStatsController.java
@@ -4,6 +4,7 @@ import com.deare.backend.api.report.dto.response.ReportStatsResponseDTO;
 import com.deare.backend.api.report.service.ReportStatsService;
 import com.deare.backend.global.auth.util.SecurityUtil;
 import com.deare.backend.global.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,10 @@ public class ReportStatsController {
 
     private final ReportStatsService reportStatsService;
 
+    @Operation(
+            summary = "리포트 통계 조회",
+            description = "통계에 필요한 데이터 반환합니다."
+    )
     @GetMapping
     public ApiResponse<ReportStatsResponseDTO> getStatics(
     ){
