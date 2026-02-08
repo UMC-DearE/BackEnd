19fa33b0ef5cddf9039f38f0241b12b3102467e6
Minsu Kwon <kms37480@naver.com>
2026-02-08 15:45:26 +0900

    [refactor]imageupload

--- src/main/java/com/deare/backend/api/image/controller/ImageController.java
+++ src/main/java/com/deare/backend/api/image/controller/ImageController.java
@@ -3,6 +3,7 @@ package com.deare.backend.api.image.controller;
 import com.deare.backend.api.image.dto.ImageUploadResponseDTO;
 import com.deare.backend.api.image.service.ImageUploadService;
 import com.deare.backend.global.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +17,14 @@ public class ImageController {
 
     private final ImageUploadService imageUploadService;
 
-    @PostMapping
+    @PostMapping(consumes = "multipart/form-data")
+    @Operation(
+            summary = "이미지 업로드",
+            description = "사용자가 이미지를 업로드하는 API입니다. 파일과 dir(profile, letter, sticker, folder)을 multipart/form-data로 전송합니다."
+    )
     public ApiResponse<ImageUploadResponseDTO> uploadImage(
             @RequestPart("file") MultipartFile file,
-            @RequestPart(value = "dir", required = false) String dir
+            @RequestPart("dir") String dir
     ) {
         return ApiResponse.success(
                 imageUploadService.uploadImage(file, dir)
