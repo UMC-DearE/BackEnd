3e09a13bbcf054d450a9ce864182025a6d70de20
Minsu Kwon <kms37480@naver.com>
2026-02-12 01:23:33 +0900

    fix 오류 수정

--- src/main/java/com/deare/backend/api/home/dto/result/HomeDashboardDTO.java
+++
@@ -1,9 +0,0 @@
-package com.deare.backend.api.home.service.dto;
-
-import java.util.List;
-
-public record HomeDashboardResult(
-        HomeUserResult user,
-        HomeSettingResult setting,
-        List<HomeStickerResult> stickers
-) {}
