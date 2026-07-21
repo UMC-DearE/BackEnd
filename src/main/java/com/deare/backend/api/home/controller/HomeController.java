37fe316013babdd413a6782fefd713c957347a90
eunwoo <enu_i@naver.com>
2026-07-21 12:05:18 +0900

    [DEPLOY] 배포 서버 반영 (#246)
    * chore: cd.yml 신뢰정책에 따른 브랜치 허용 코드 각주 처리 (#236)
    
    Co-authored-by: catomat0 <catomat0@github.com>
    
    * refactor: 홈 화면 로직 변경
    
    * test 파일 삭제
    
    * fix: 폴더 수정 로직 변경
    
    * pr 리뷰 수정
    
    * term 충돌 해결
    
    ---------
    
    Co-authored-by: 김동국 <catomatot@gmail.com>
    Co-authored-by: catomat0 <catomat0@github.com>
    Co-authored-by: kwonminsooo <kms37480@naver.com>
    Co-authored-by: kwonminsooo <118319151+kwonminsooo@users.noreply.github.com>

--- src/main/java/com/deare/backend/api/home/controller/HomeController.java
+++ src/main/java/com/deare/backend/api/home/controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.deare.backend.api.home.controller;
 
+import com.deare.backend.api.home.dto.request.HomeEditRequestDTO;
 import com.deare.backend.api.home.dto.response.HomeDashboardResponse;
 import com.deare.backend.api.home.service.HomeService;
 import com.deare.backend.global.auth.util.SecurityUtil;
 import com.deare.backend.global.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,4 +27,16 @@ public class HomeController {
         HomeDashboardResponse res = homeService.getHome(userId);
         return ApiResponse.success(res);
     }
+
+    @PutMapping
+    @Operation(
+            summary = "홈 화면 편집 저장",
+            description = "스티커 배치 , 배경색 변경 등 홈화면 편집 내용을 저장하는 API입니다."
+    )
+    public ApiResponse<Void> editHome(@Valid @RequestBody HomeEditRequestDTO request){
+        Long userId = SecurityUtil.getCurrentUserId();
+        homeService.editHome(userId, request);
+        return ApiResponse.success(null);
+    }
+
 }
