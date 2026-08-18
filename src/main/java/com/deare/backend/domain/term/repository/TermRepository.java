38b283a03dff8a4e2224c4058db42183cb4b16d3
김동국 <catomatot@gmail.com>
2026-08-18 22:10:01 +0900

    [FEAT] 마이페이지용 약관 조회 API 및 약관 수정 (#276)
    * feat: 현행 약관 조회 API 구현
    
    * chore: 런칭 전 약관 내용 수정
    
    ---------
    
    Co-authored-by: catomat0 <catomat0@github.com>

--- src/main/java/com/deare/backend/domain/term/repository/TermRepository.java
+++ src/main/java/com/deare/backend/domain/term/repository/TermRepository.java
@@ -1,6 +1,7 @@
 package com.deare.backend.domain.term.repository;
 
 import com.deare.backend.domain.term.entity.Term;
+import com.deare.backend.domain.term.entity.enums.TermType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -18,4 +19,9 @@ public interface TermRepository extends JpaRepository<Term, Long> {
      * 활성화된 약관 목록 조회 (회원가입용)
      */
     List<Term> findByIsActiveTrue();
+
+    /**
+     * 활성화된 약관 목록 조회 - 타입 필터
+     */
+    List<Term> findByIsActiveTrueAndType(TermType type);
 }
