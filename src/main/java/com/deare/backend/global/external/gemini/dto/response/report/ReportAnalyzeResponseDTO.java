0aa7b9f3e99b6619ebd022da81d671d11dec47bb
hajimeong <ha_01@naver.com>
2026-08-20 23:53:01 +0900

    feat: 리포트 편지 분석 응답 형태 선언

---
+++ src/main/java/com/deare/backend/global/external/gemini/dto/response/report/ReportAnalyzeResponseDTO.java
@@ -0,0 +1,12 @@
+package com.deare.backend.global.external.gemini.dto.response.report;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReportAnalyzeResponseDTO {
+    private String description;
+    private String hashtag1;
+    private String hashtag2;
+}
