f52e95f07a9f9511df9cd550aea0b60d3751c67f
ownue <ownue.lee@gmail.com>
2026-08-25 10:59:08 +0900

    fix: 문법 오류 수정

--- src/main/resources/db/migration/V11__add_invite_benefit_guide_status.sql
+++ src/main/resources/db/migration/V11__add_invite_benefit_guide_status.sql
@@ -1,3 +1,3 @@
 ALTER TABLE user_setting
-    ADD COLUMN invite_benefit_guide_status VARCHAR(20) NOT NULL DEFAULT ''NOT_ELIGIBLE''
+    ADD COLUMN invite_benefit_guide_status VARCHAR(20) NOT NULL DEFAULT 'NOT_ELIGIBLE'
         AFTER membership_plan;
