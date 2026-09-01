101a3362d77d4db3ed6cce5dfb852fb59228ae56
ownue <ownue.lee@gmail.com>
2026-09-01 09:56:59 +0900

    fix: content 삭제

--- src/main/resources/db/migration/V17__require_complete_letter_content_encryption.sql
+++ src/main/resources/db/migration/V17__require_complete_letter_content_encryption.sql
@@ -3,4 +3,5 @@ ALTER TABLE letter
     MODIFY COLUMN content_encryption_nonce VARCHAR(16)
         CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
     MODIFY COLUMN content_encryption_key_version INT NOT NULL,
-    MODIFY COLUMN content_encryption_format_version INT NOT NULL;
+    MODIFY COLUMN content_encryption_format_version INT NOT NULL,
+    DROP COLUMN content;
